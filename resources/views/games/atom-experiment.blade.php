1bae68edfd238aae6494487119d1e6d0f74f2fe6
Muhammad Zhafran Ammar <zhafran.ammar@suitmedia.com>
2023-05-22 22:54:58 +0700

    Update page

--- resources/views/games/atom-experiment.blade.php
+++ resources/views/games/atom-experiment.blade.php
@@ -1,9 +1,29 @@
 <x-app-layout>
     <x-slot name="header">
-      <script src="https://cdn.jsdelivr.net/npm/phaser@3.50/dist/phaser.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/phaser@3.50/dist/phaser.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Atom Experiment') }}
         </h2>
+        <script>
+        var user = @json(Auth::user());
+        let level = user.current_level;
+        // get scenes data from hit api endpoint /api/scenes
+        axios.get('/api/scenes/' + level)
+            .then(response => {
+            // console.log(response.data); // Lakukan sesuatu dengan data yang diterima
+            const gameScript = response.data.detail.src;
+            // console.log(gameScript);
+            // append gameScript as src script to game-container
+            const gameContainer = document.getElementById('game-container');
+            const scriptElement = document.createElement('script');
+            scriptElement.src = gameScript;
+            gameContainer.appendChild(scriptElement);
+            })
+            .catch(error => {
+            console.error('Terjadi kesalahan:', error);
+            });
+        </script>
     </x-slot>
 
     <div class="py-12">
--- resources/views/games/build-the-atom.blade.php
+++ resources/views/games/build-the-atom.blade.php
@@ -28,7 +28,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-yellow-500 overflow-hidden shadow-sm sm:rounded-lg" id="game-container">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg" id="game-container">
             </div>
             {{-- <a href="{{ route('next-level') }}">Next Level</a> --}}
         </div>
