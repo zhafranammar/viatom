92f2b12bc5c5625cbb80527c07230b85cabbe8ea
Yopi Julia Nurriski <yopijulia172@gmail.com>
2023-06-19 22:13:24 +0700

    edit discussion

--- resources/views/discussion/create.blade.php
+++ resources/views/discussion/create.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
-<body style="background-image: url('{{ asset('assets/web/map1.png') }}'); background-size: cover;">
+<body class="bg-cover" style="background-image: url('{{ asset('assets/web/map1.png') }}');">
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg" id="game-container">
@@ -32,28 +32,35 @@
                     <img src="assets/web/logo.png" alt="" class="mr-2 w-16">
                     <img src="assets/web/viatom-text.png" alt="" class="w-48">
                 </div>
-
+                
                 <div class="mt-4 flex flex-col items-center relative">
-                    <div class="bg-#D9D9D9 px-8 mt-8 pb-8 w-full max-w-screen-lg flex flex-col items-center relative" id="discussion-container">
-                        <h1>Create Discussion</h1>
-                        <form method="POST" action="/discussions">
-                            <label for="title">Title:</label><br>
-                            <input type="text" id="title" name="title"><br><br>
+                    <div class="top-bg px-8 mt-8 pt-4 pb-4 w-full max-w-screen-lg flex flex-col items-center relative text-white rounded-t-lg">
+                        <h1 class="text-white">Create Discussion</h1>
+                    </div>
+                </div>
+
+                <div class="flex flex-col items-center relative">
+                    <div class="bg-gray-200 px-8 pb-8 w-full max-w-screen-lg flex flex-col items-start relative" id="discussion-container">
+                        
+                        <form method="POST" action="/discussions" class="mt-4">
+                            @csrf
+                            <label for="title" class="block">Title:</label>
+                            <input type="text" id="title" name="title" class="mt-1 mb-4 px-2 py-1 border border-gray-400 rounded-md w-full">
                             
-                            <label for="body">Body:</label><br>
-                            <textarea id="body" name="body"></textarea><br><br>
+                            <label for="body" class="block">Body:</label>
+                            <textarea id="body" name="body" class="mt-1 mb-4 px-2 py-1 border border-gray-400 rounded-md w-full"></textarea>
                             
-                            <button type="submit">Submit</button>
+                            <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-md">Submit</button>
                         </form>
                     </div>
                 </div>
                 <div class="fixed bottom-0 left-0 right-0 flex items-center justify-center pb-5">
                     <div class="flex btn-game align-center rounded-md p-2">
                         <div class="shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-[#F2DC52] stroke-0 text-center xl:p-2.5">
-                            <span class="material-symbols-outlined" style="color: #F2DC52;"> forum</span>
+                            <span class="material-symbols-outlined" style="color: #F2DC52;">forum</span>
                         </div>
                         <h1 class="p-2 font-bold font-mono text-2xl" style="color: #F2DC52;">Discussion</h1>
-                    </div>
+                    </div>    
                 </div>
             </div>
         </div>
@@ -66,20 +73,31 @@
         font-family: 'Material Symbols Outlined';
         font-size: 36px;
         color: #F2DC52;
-        font: bold;
+        font-weight: bold;
     }
     .btn-game {
         background-color: #636357;
         display: flex;
         justify-content: center;
-        align-items: center;
+        align-items: center; 
     }
     #discussion-container {
         background-color: #D9D9D9;
     }      
     h1 {
-        font-size: 24px; /* Ubah ukuran sesuai keinginan Anda */
+        font-size: 24px;
         font-weight: bold;
     }
-    
+    #game-container {
+        position: absolute;
+        width: 1280px;
+        height: 720px;
+        left: calc(50% - 1280px/2 - 0.5px);
+        top: calc(50% - 720px/2 + 17px);
+        background: #FFFFFF;
+        border-radius: 16px;
+    }
+    .top-bg {
+    background-color: #636357;
+    }
 </style>
--- resources/views/discussion/show.blade.php
+++ resources/views/discussion/show.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<script src="https://cdn.jsdelivr.net/npm/phaser@3.50/dist/phaser.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/phaser@3.50/dist/phaser.min.js"></script>
     <title>Discussion Detail</title>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
@@ -14,7 +14,6 @@
 <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg" id="game-container">
-
                 <div class="relative">
                     <a href="{{ route('dashboard') }}" class="btn-game px-2 py-2 rounded-lg absolute left-0 top-0 m-5">
                         <span class="material-symbols-outlined">arrow_back</span>
@@ -28,43 +27,41 @@
                         </a>
                     </div>
                 </div>
-
                 <div class="pt-4 mx-auto sm:px-6 lg:px-8 flex justify-center items-center bg-white overflow-hidden">
                     <img src="assets/web/logo.png" alt="" class="mr-2 w-16">
                     <img src="assets/web/viatom-text.png" alt="" class="w-48">
                 </div>
-
                 <div class="mt-4 flex flex-col items-center relative">
-                    <div class="bg-#D9D9D9 px-8 mt-8 pb-8 w-full max-w-screen-lg flex flex-col items-center relative" id="discussion-container">
-                        <h1 class="text-xl font-bold mb-4">Discussion Detail</h1>
+                    <div class="top-bg px-8 mt-8 pt-4 pb-4 w-full max-w-screen-lg flex flex-col items-center relative text-white rounded-t-lg">
+                        <h1 class="text-white">Discussion Detil</h1>
+                    </div>
+                </div>
+                <div class="flex flex-col items-center relative">
+                    <div class="bg-gray-300 px-8 pb-8 w-full max-w-screen-lg flex flex-col items-start relative" id="discussion-container">
                         <h2>{{ $discussion->topic }}</h2>
                         <p>{{ $discussion->body }}</p>
                         <hr>
-                        
                         <h3>Replies</h3>
                         <ul>
                             @if ($discussion->replies)
                             @foreach ($discussion->replies as $reply)
-                                <ul>
-                                    <li>{{ $reply->user->name }}</li>
-                                    <li>{{ $reply->body }}</li>
-                                    <li>{{ $reply->created_at}}</li>
-                                </ul>
+                            <ul>
+                                <li>{{ $reply->user->name }}</li>
+                                <li>{{ $reply->body }}</li>
+                                <li>{{ $reply->created_at}}</li>
+                            </ul>
                             @endforeach
                             @endif
                         </ul>
-                        
                         <h3>Add Reply</h3>
                         <form method="POST" action="/discussions/{{ $discussion->id }}/replies">
                             @csrf
                             <label for="reply">Reply:</label><br>
                             <textarea id="reply" name="body"></textarea><br><br>
-                            
                             <button type="submit">Submit</button>
                         </form>
                     </div>
                 </div>
-
                 <div class="fixed bottom-0 left-0 right-0 flex items-center justify-center pb-5">
                     <div class="flex btn-game align-center rounded-md p-2">
                         <div class="shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-[#F2DC52] stroke-0 text-center xl:p-2.5">
@@ -79,26 +76,33 @@
 </body>
 </html>
 
-
 <style>
     .material-symbols-outlined {
         font-family: 'Material Symbols Outlined';
         font-size: 36px;
         color: #F2DC52;
-        font: bold;
+        font-weight: bold;
     }
     .btn-game {
         background-color: #636357;
         display: flex;
         justify-content: center;
         align-items: center;
-    }
-    #discussion-container {
-        background-color: #D9D9D9;
-    }      
+    }     
     h1 {
-        font-size: 24px; /* Ubah ukuran sesuai keinginan Anda */
+        font-size: 24px;
         font-weight: bold;
     }
-    
+    #game-container {
+        position: absolute;
+        width: 1280px;
+        height: 720px;
+        left: calc(50% - 1280px/2 - 0.5px);
+        top: calc(50% - 720px/2 + 17px);
+        background: #FFFFFF;
+        border-radius: 16px;
+    }
+    .top-bg {
+    background-color: #636357;
+    }
 </style>
